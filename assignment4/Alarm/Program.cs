e5c206e5ffdaa4081fdf5c8a8efd63b78eac5672
Zihautanejo <126685670+Zihautanejo@users.noreply.github.com>
2023-03-17 12:36:43 +0800

    Add files via upload

---
+++ assignment4/Alarm/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+//使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+//在闹钟走时时或者响铃时，在控制台显示提示信息。
+
+namespace Alarm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock c = new Clock();
+            Time AlarmTime = new Time() { Minute = 16, Hour = 11 };
+            Time TickTime = new Time() { Minute = 1, Hour = 0 };
+            c.Work(AlarmTime, TickTime);
+            Console.ReadKey();
+        }
+    }
+    public delegate void ClockHandler(object clocker, Time AlarmTime,Time TickTime);
+    public class Time
+    {
+        private int min, hou;
+        public int Minute
+        {
+            get { return min; }
+            set
+            {
+                if (value >= 0 && value <= 60)
+                    min = value;
+                else
+                    throw new Exception("时间的分钟设置出错");
+            }
+        }
+        public int Hour {
+            get { return hou; }
+            set
+            {
+                if (value >= 0 && value <= 24)
+                    hou = value;
+                else
+                    throw new Exception("时间的时钟设置出错");
+            }
+        }
+    }
+    public class Clock
+    {
+        static Time time;
+        static bool whether = false;
+        public event ClockHandler OnWork;
+        public static Time endtime = new Time() { Minute = 60,Hour = 24};
+        public void Work(Time AlarmTime,Time TickTime)
+        //设定闹钟此时的时间,设定要响铃的时间,时隔多少响铃的时间
+        {
+            time = GetTime();
+            while (GetTime() != endtime)
+            {
+                //OnWork += new ClockHandler(Alarm);
+                //OnWork += Tick;
+                //OnWork(this, AlarmTime, TickTime);//谁声明，谁触发
+                Alarm(this, AlarmTime, TickTime);
+                Tick(this, AlarmTime, TickTime);
+            }
+        }
+        public static void Tick(object cloker,Time AlarmTime,Time TickTime)
+        {
+            if (time.Minute + TickTime.Minute == GetTime().Minute &&
+                time.Hour + TickTime.Hour == GetTime().Hour)
+            {
+                Console.WriteLine("Tick! Now the time is " + GetTime().Hour + ":" + GetTime().Minute);
+                time = GetTime();
+            } 
+        }
+        public static void Alarm(object cloker,Time AlarmTime,Time TickTime)
+        {   
+            //保证只响一次
+            if (GetTime().Minute == AlarmTime.Minute &&
+                GetTime().Hour == AlarmTime.Hour && whether == false)
+            {
+                whether = true;
+                Console.WriteLine("Alarm! Now the time is " + AlarmTime.Hour + ":" + AlarmTime.Minute);
+            }
+        }
+        public static Time GetTime() //获取系统的时间
+        {
+            Time time = new Time();
+            string s = System.DateTime.Now.ToString("t");
+            time.Hour = Int32.Parse(s.Substring(0,2));
+            time.Minute = Int32.Parse(s.Substring(3, 2));
+            return time;
+        }
+    }
+}
---
+++ assignment4/ListCalculate/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCalculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            //传入list的数据
+            list.Add(1); list.Add(3); list.Add(5);
+            //lambda表达式，用局部变量来传结果
+            int sum = 0;int min = list.Head.Data;int max = list.Head.Data;
+            GenericList<int>.Calculate(list, m=>Console.WriteLine(m.Data));
+            GenericList<int>.Calculate(list, m => sum+=m.Data);
+            GenericList<int>.Calculate(list, m => max=Math.Max(max,m.Data));
+            GenericList<int>.Calculate(list, m => min=Math.Min(min, m.Data));
+            Console.WriteLine("该链表的总和为" + sum);
+            Console.WriteLine("该链表的最大值为" + max);
+            Console.WriteLine("该链表的最小值为" + min);
+            Console.ReadKey();
+        }
+    }
+    //泛型节点类
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+
+        }
+        public static void Calculate(GenericList<T> list, Action<Node<T>> action) {
+            Node<T> p = list.head;
+            while (p!= null)
+            {
+                action(p);
+                p = p.Next;
+            }
+        }
+    } 
+}
